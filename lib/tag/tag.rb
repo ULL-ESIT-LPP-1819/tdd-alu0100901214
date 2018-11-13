869051430a19abf02ceef8506b7bbc1741bec03c
Sergio González <alu0100901214@ull.edu.es>
2018-11-13 12:28:00 +0000

    Espero que la variable Tag.name_tag tenga como valor 'Pedro', falla.

--- lib/tag/tag.rb
+++
@@ -1,8 +0,0 @@
-
-	class Nutrition
-		attr_accesor :name_tag, :energic_value, :q_fats, :q_fats_sat, :HC, :sugars, :potrein, :salt
-
-		def initialize()
-		end
-	end
-
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,9 +1,13 @@
+
 RSpec.describe Tag do
   it "has a version number" do
     expect(Tag::VERSION).not_to be nil
   end
-
   it "does something useful" do
     expect(false).to eq(false)
   end
+  it "name_tag = 'Pedro'" do
+    expect(Tag.name_tag).to eq("Pedro")
+  end
 end
+
