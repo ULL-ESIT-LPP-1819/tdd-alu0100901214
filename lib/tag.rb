a741c5beb317746223dd916f80c05c9199efc814
Sergio González <alu0100901214@ull.edu.es>
2018-11-14 12:23:43 +0000

    La cantidad de grasas saturadas funciona correctamente

--- lib/tag.rb
+++ lib/tag.rb
@@ -2,9 +2,9 @@ require "tag/version"
 
 #module Tag
   class Nutrition
-	  attr_accessor :name_tag, :energetic_value, :q_fats
-	  def initialize(name_tag, energetic_value,q_fats)
-		  @name_tag, @energetic_value, @q_fats = name_tag, energetic_value, q_fats
+	  attr_accessor :name_tag, :energetic_value, :q_fats, :q_fats_sat
+	  def initialize(name_tag, energetic_value, q_fats, q_fats_sat)
+		  @name_tag, @energetic_value, @q_fats, @q_fats_sat = name_tag, energetic_value, q_fats, q_fats_sat
 	  end
   end
 
