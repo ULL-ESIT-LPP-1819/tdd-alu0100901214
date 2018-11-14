276ab12ccb8f84e58214284acb9f29e1988c38bf
Sergio González <alu0100901214@ull.edu.es>
2018-11-14 12:27:28 +0000

    La prueba de carbohidratos funciona correctamente

--- lib/tag.rb
+++ lib/tag.rb
@@ -2,9 +2,9 @@ require "tag/version"
 
 #module Tag
   class Nutrition
-	  attr_accessor :name_tag, :energetic_value, :q_fats, :q_fats_sat
-	  def initialize(name_tag, energetic_value, q_fats, q_fats_sat)
-		  @name_tag, @energetic_value, @q_fats, @q_fats_sat = name_tag, energetic_value, q_fats, q_fats_sat
+	  attr_accessor :name_tag, :energetic_value, :q_fats, :q_fats_sat, :hc
+	  def initialize(name_tag, energetic_value, q_fats, q_fats_sat, hc)
+		  @name_tag, @energetic_value, @q_fats, @q_fats_sat, @hc = name_tag, energetic_value, q_fats, q_fats_sat, hc
 	  end
   end
 
