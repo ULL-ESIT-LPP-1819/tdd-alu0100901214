01ff7495617eeea30b6d56b2888ce616cfb5e881
Sergio González <alu0100901214@ull.edu.es>
2018-11-14 11:43:50 +0000

    La prueba del valor energético falla

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,14 +1,18 @@
 require './lib/tag'
 RSpec.describe Tag do
   it "has a version number" do
-    expect(Tag::VERSION).not_to be nil
+  	expect(Tag::VERSION).not_to be nil
   end
   it "does something useful" do
-    expect(false).to eq(false)
+  	expect(false).to eq(false)
   end
   @@tag1 = Nutrition.new("Pedro")
   it "name_tag se almacena correctamente conteniendo la palabra 'Pedro'" do
-    expect(@@tag1.name_tag).to eq("Pedro")
+  	expect(@@tag1.name_tag).to eq("Pedro")
+  end
+
+  it "energetic_value se almacena correctamente conteniendo la variable" do
+	expect(@@tag1.energetic_value).to eq(98)
   end
 end
 
