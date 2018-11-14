d85ceb577f5802f80141b701ce91ba21b571cf03
Sergio González <alu0100901214@ull.edu.es>
2018-11-14 12:30:18 +0000

    La prueba de los azucares funciona correctamente

--- lib/tag.rb
+++ lib/tag.rb
@@ -2,9 +2,9 @@ require "tag/version"
 
 #module Tag
   class Nutrition
-	  attr_accessor :name_tag, :energetic_value, :q_fats, :q_fats_sat, :hc
-	  def initialize(name_tag, energetic_value, q_fats, q_fats_sat, hc)
-		  @name_tag, @energetic_value, @q_fats, @q_fats_sat, @hc = name_tag, energetic_value, q_fats, q_fats_sat, hc
+	  attr_accessor :name_tag, :energetic_value, :q_fats, :q_fats_sat, :hc, :sugars
+	  def initialize(name_tag, energetic_value, q_fats, q_fats_sat, hc, sugars)
+		  @name_tag, @energetic_value, @q_fats, @q_fats_sat, @hc, @sugars = name_tag, energetic_value, q_fats, q_fats_sat, hc, sugars
 	  end
   end
 
