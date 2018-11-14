263baf6ee0d9a68bb033bb7973076a2744d9aa19
Sergio González <alu0100901214@ull.edu.es>
2018-11-14 12:37:52 +0000

    La prueba de la sal funciona correctamente

--- lib/tag.rb
+++ lib/tag.rb
@@ -2,9 +2,9 @@ require "tag/version"
 
 #module Tag
   class Nutrition
-	  attr_accessor :name_tag, :energetic_value, :q_fats, :q_fats_sat, :hc, :sugars, :protein
-	  def initialize(name_tag, energetic_value, q_fats, q_fats_sat, hc, sugars, protein)
-		  @name_tag, @energetic_value, @q_fats, @q_fats_sat, @hc, @sugars, @protein = name_tag, energetic_value, q_fats, q_fats_sat, hc, sugars, protein
+	  attr_accessor :name_tag, :energetic_value, :q_fats, :q_fats_sat, :hc, :sugars, :protein, :salt
+	  def initialize(name_tag, energetic_value, q_fats, q_fats_sat, hc, sugars, protein, salt)
+		  @name_tag, @energetic_value, @q_fats, @q_fats_sat, @hc, @sugars, @protein, @salt = name_tag, energetic_value, q_fats, q_fats_sat, hc, sugars, protein, salt
 	  end
   end
 
